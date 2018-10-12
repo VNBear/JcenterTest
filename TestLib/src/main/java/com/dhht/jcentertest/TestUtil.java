ab10d0106d3f52ec8e1d1e824158e48e2696832a
ZouWei <791029369@qq.com>
2018-10-12 15:04:05 +0800

    Initial commit

---
+++ TestLib/src/main/java/com/dhht/jcentertest/TestUtil.java
@@ -0,0 +1,34 @@
+package com.dhht.jcentertest;
+
+// ┏┓　　　┏┓
+// ┏┛┻━━━┛┻┓
+// ┃　　　　　　　┃ 　
+// ┃　　　━　　　┃
+// ┃　┳┛　┗┳　┃
+// ┃　　　　　　　┃
+// ┃　　　┻　　　┃
+// ┃　　　　　　　┃
+// ┗━┓　　　┏━┛
+// ┃　　　┃ 神兽保佑　　　　　　　　
+// ┃　　　┃ 代码无BUG！
+// ┃　　　┗━━━┓
+// ┃　　　　　　　┣┓
+// ┃　　　　　　　┏┛
+// ┗┓┓┏━┳┓┏┛
+// ┃┫┫　┃┫┫
+// ┗┻┛　┗┻┛
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * CreateDate：2018/10/12
+ * Creator： VNBear
+ * Description:
+ **/
+public class TestUtil {
+
+    public static void sayGoodbye(Context context){
+        Toast.makeText(context.getApplicationContext(), " hello world", Toast.LENGTH_SHORT).show();
+    }
+}
